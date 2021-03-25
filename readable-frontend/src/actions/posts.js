c2565d65553ebd7063fa5eb6b3583325522c2dfa
Taina Conde <tainabatistaconde@gmail.com>
2021-03-25 16:41:04 -0400

    feat: Actions - create actions for posts
    Create actions to add post, receive
    the posts, delete a post, save vote
    to a post, edit a post, and incre-
    ment the comment counter of a post.

--- readable-frontend/src/actions/posts.js
+++ readable-frontend/src/actions/posts.js
@@ -2,6 +2,8 @@ export const ADD_POST = 'ADD_POST'
 export const RECEIVE_POSTS = 'RECEIVE_POSTS'
 export const DELETE_POST = 'DELETE_POST'
 export const SAVE_VOTE = 'SAVE_VOTE'
+export const EDIT_POST = 'EDIT_POST'
+export const INCREMENT_COMMENT_COUNTER = 'INCREMENT_COMMENT_COUNTER'
 
 export function addPost(post) {
     return {
@@ -9,3 +11,36 @@ export function addPost(post) {
         post
     }
 }
+export function receivePosts(posts) {
+    return {
+        type: RECEIVE_POSTS,
+        posts
+    }
+}
+export function deletePost(id) {
+    return {
+        type: DELETE_POST,
+        id
+    }
+}
+export function saveVoteToPost(id, option) {
+    return {
+        type: SAVE_VOTE,
+        id,
+        option
+    }
+}
+export function editPost(id, post) {
+    return {
+        type: EDIT_POST,
+        id,
+        post
+    }
+}
+export function incrementComment(id, count) {
+    return {
+        type: INCREMENT_COMMENT_COUNTER,
+        count
+    }
+}
+
