94fbb6b24a2d20d3d28d4264ffd6d8c1887164ed
Taina Conde <tainabatistaconde@gmail.com>
2021-03-25 17:40:03 -0400

    feat: Actions - comments actions
    Copy posts actions and adapt for comments.
    Remove incrementComments and add delete
    Parent.

---
+++ readable-frontend/src/actions/comments.js
@@ -0,0 +1,46 @@
+export const ADD_COMMENT = 'ADD_COMMENT'
+export const RECEIVE_COMMENTS = 'RECEIVE_COMMENTS'
+export const DELETE_COMMENT = 'DELETE_COMMENT'
+export const SAVE_VOTE = 'SAVE_VOTE'
+export const EDIT_COMMENT = 'EDIT_COMMENT'
+export const DELETE_PARENT = 'DELETE_PARENT'
+
+export function addComment(comment) {
+    return {
+        type: ADD_COMMENT,
+        comment
+    }
+}
+export function receiveComments(comments) {
+    return {
+        type: RECEIVE_COMMENTS,
+        comments
+    }
+}
+export function deleteComments(id) {
+    return {
+        type: DELETE_COMMENT,
+        id
+    }
+}
+export function saveVoteToComment(id, option) {
+    return {
+        type: SAVE_VOTE,
+        id,
+        option
+    }
+}
+export function editComment(id, comment) {
+    return {
+        type: EDIT_COMMENT,
+        id,
+        comment
+    }
+}
+export function deleteParent(id) {
+    return {
+        type: DELETE_PARENT,
+        id
+    }
+}
+
