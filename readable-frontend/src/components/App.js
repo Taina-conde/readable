581d8d493fde963a77ba0f478ac7de788d8c423f
Taina Conde <tainabatistaconde@gmail.com>
2021-03-25 18:20:42 -0400

    feat: App - route to correct components
    Add Routes to navigate to correct components depending
    on the path. Create NotFound component for when the user
    types an invalid address.

--- readable-frontend/src/components/App.js
+++ readable-frontend/src/components/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container } from 'react-bootstrap'
 import Navi from './Navi';
+import HomeView from './HomeView'
+import CategoriesView from './CategoriesView'
+import PostDetailsView from './PostDetailsView'
+import CreateEditView from './CreateEditView'
+import NotFound from './NotFound'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
@@ -9,6 +15,15 @@ class App extends React.Component {
       <Router >
         <React.Fragment>
           <Navi/>
+          <Container>
+            <Switch>
+              <Route path = '/' exact component = {HomeView}/>
+              <Route path = '/:category' exact component = {CategoriesView}/>
+              <Route path = '//:category/:post_id' exact component = {PostDetailsView}/>
+              <Route path = '/add' exact component = {CreateEditView}/>
+              <Route path = '/' component = {NotFound}/>
+            </Switch>
+          </Container>
           
         </React.Fragment>
         
--- readable-frontend/src/components/HomeView.js
+++ readable-frontend/src/components/HomeView.js
@@ -2,7 +2,9 @@ import React from 'react';
 class HomeView extends React.Component {
     render(){
         return(
-            <div></div>
+            <div>
+                
+            </div>
         )
     }
 }
