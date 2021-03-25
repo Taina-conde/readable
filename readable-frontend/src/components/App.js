a4d6ba13873a9e65420f3f8a345af09710fdf8f3
Taina Conde <tainabatistaconde@gmail.com>
2021-03-25 15:25:47 -0400

    feat: install packages
    Install react-bootstrap, bootstrap,
    react-router-dom, redux, react-redux.

--- readable-frontend/src/components/App.js
+++ readable-frontend/src/components/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div >
-      
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <Router >
+        <React.Fragment>
+          
+        </React.Fragment>
+        
+      </Router>
+    );
+  }
 }
 
 export default App;
