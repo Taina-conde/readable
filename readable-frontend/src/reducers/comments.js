6f60465b297acc65e090114ceb1fc38a2ce3b738
Taina Conde <tainabatistaconde@gmail.com>
2021-03-25 17:51:08 -0400

    feat: Reducers - adjust the way it changes the redux store
    Adjust the way the redux store is updated
    considering that the actions are related
    to comments part of the store.

---
+++ readable-frontend/src/reducers/comments.js
@@ -0,0 +1,55 @@
+import {
+    RECEIVE_COMMENTS,
+    ADD_COMMENT,
+    EDIT_COMMENT,
+    SAVE_VOTE,
+    DELETE_PARENT,
+    DELETE_COMMENT
+} from '../actions/posts'
+
+export default postsReducer(state ={}, action) {
+    switch(action.type) {
+        case RECEIVE_COMMENTS: 
+            return {
+                ...state,
+                ...action.comments
+            }
+        case ADD_COMMENT : 
+            return {
+                ...state,
+                [action.comment.id] : action.comment
+            }
+        case EDIT_COMMENT:
+            return {
+                ...state,
+                [action.comment.id] : action.comment
+            }
+        case SAVE_VOTE: 
+            return {
+                ...state,
+                [action.id] : {
+                    ...state[action.id],
+                    voteScore : action.option === 'upVote' 
+                        ? state[action.id].voteScore + 1
+                        : state[action.id].voteScore - 1
+                }
+            }
+        case DELETE_PARENT: 
+            return {
+                ...state,
+                [action.id]: {
+                    ...state[action.id],
+                    parentDeleted: true
+                }
+            }
+        case DELETE_COMMENT: 
+            return {
+                ...state,
+                [action.id] : {
+                    ...state[action.id],
+                    deleted: true
+                }
+            }
+        
+    }
+}
