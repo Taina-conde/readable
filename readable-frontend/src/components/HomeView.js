9d332b627f6584e96dc8b68e0bcb62788df11be8
Taina Conde <tainabatistaconde@gmail.com>
2021-03-26 16:22:19 -0400

    fix: HomeView - warning to add key prop to list item
    Fix warning by mapping postsIds to posts keys
    and rendering the list item qith key equal to
    each id in the postsIds array.

--- readable-frontend/src/components/HomeView.js
+++ readable-frontend/src/components/HomeView.js
@@ -5,13 +5,17 @@ import PostDetailsView from './PostDetailsView';
 
 class HomeView extends React.Component {
     render(){
-        const {posts} = this.props;
+        const {postsIds, posts} = this.props;
+        console.log('homeview posts', posts)
         return(
             <ul>
-                {Object.keys(posts).map((post) => (
-                    <Link to = {`/${post.category}/${post.id}`} key = {post.id}>
-                        <PostDetailsView id = {post.id}/>
-                    </Link>
+                {postsIds.map((id) => (
+                    <li key = {id}>
+                        <Link to = {`/${posts[id].category}/${id}`} >
+                            <PostDetailsView id = {id}/>
+                        </Link>
+                    </li>
+                    
                 ))}
             </ul>
         )
@@ -19,6 +23,7 @@ class HomeView extends React.Component {
 }
 function mapStateToProps({posts}) {
     return {
+        postsIds: Object.keys(posts),
         posts
     }
 }
