ffe9e40476a24a034d577fd573d51d7c93c15c3e
Taina Conde <tainabatistaconde@gmail.com>
2021-03-26 11:45:30 -0400

    feat: Reducers - add categories reducer
    Build up categories reducer with RECEIVE_CATEGORIES
    action type and default cases. Import categories
    reducer in index file and combine it to the other
    reducers.

--- readable-frontend/src/reducers/index.js
+++ readable-frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux' 
 import commentsReducer from './comments'
 import postsReducer from './posts'
+import categoriesReducer from './categories'
 
 export default combineReducers({
     comments: commentsReducer,
-    posts : postsReducer
+    posts : postsReducer,
+    categories: categoriesReducer
 })
