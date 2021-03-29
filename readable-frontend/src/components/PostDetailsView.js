3535581f4d8cea3545a61487a99a4f8a34bf9729
Taina Conde <tainabatistaconde@gmail.com>
2021-03-29 16:12:13 -0400

    style: PostDetailsView - add comments title

--- readable-frontend/src/components/PostDetailsView.js
+++ readable-frontend/src/components/PostDetailsView.js
@@ -1,48 +1,79 @@
 import React from 'react'; 
 import {Container, Row, Col} from 'react-bootstrap'
 import {connect} from 'react-redux'
+import {formatDate} from '../utils/helpers'
+import { IoChatbubblesOutline } from "react-icons/io5"
+import { BiLike, BiDislike, BiEdit, BiTrash } from 'react-icons/bi'
 class PostDetailsView extends React.Component {
     render(){
-        const {posts, id} = this.props
-        const post = posts[id]
+        const { post} = this.props
+        
         return(
             <Container>
-                <Row>
-                    <Col>
-                        {post.author}
-                    </Col>
-                    <Col>
-                        {post.timestamp}
-                    </Col>
+                <Container className ='post-box'>
+                    <Row>
+                        <Col xs = '8' className = 'post-title'>
+                            {post.title}
+                            
+                        </Col>
+                        <Col xs ='4' className = 'post-details d-flex p-0 align-items-center'>
+                            <span className = 'col-4 p-0 text-center post-author'>
+                                {`@${post.author}`}
+                            </span> 
+                            <span className = 'col-1 p-0 text-center'>
+                                •
+                            </span>
+                            <span className = 'col-7 p-0 text-center'>
+                                {formatDate(post.timestamp)}
+                            </span>
+                            
+                        </Col>
+                        
+                    </Row>
+                    <Row>
+                        <Col className= 'post-body mb-3'>
+                            {post.body}
+                        </Col>
+                    </Row>
                     
-                </Row>
-                <Row>
-                    <Col>
-                        {post.body}
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                    REPLY / COMMENT + number of comments
-                    </Col>
-                    <Col>
-                    UPVOTE/ DOWNVOTE + score
-                    </Col>
-                    <Col>
-                    EDIT
-                    </Col>
-                    <Col>
-                    DELETE
-                    </Col>
-                </Row>
+                    <Row>
+                        <Col>
+                            <IoChatbubblesOutline className = 'text-success mr-1'/>
+                            <span className = 'post-details'>{`${post.commentCount} ${post.commentCount === 1 ? 'comment': 'comments'}`}</span>
+                        </Col>
+                        <Col>
+                            <BiDislike className = 'text-success mr-1'/>
+                            <span className = 'post-vote'>{post.voteScore}</span>
+                            <BiLike className = 'text-success ml-1'/>
+                        
+                        </Col>
+                        <Col>
+                            <BiEdit className = 'text-success'/>
+                        </Col>
+                        <Col>
+                            <BiTrash className = 'text-success'/>
+                        </Col>
+                    </Row>
 
+                </Container>
+                <Container >
+                    <Row className = 'comments-title'>
+                        <Col xs='5' className = 'comments-hr p-0'><hr/></Col>
+                        <Col xs = '2'>
+                            Comments
+                        </Col>
+                        <Col xs = '5' className = 'comments-hr p-0'><hr/></Col>
+                    </Row>
+                </Container>
+                
             </Container>
         )
     }
 }
-function mapStateToProps({posts}) {
+function mapStateToProps({posts}, {match}) {
+    const id = match.params.post_id
     return {
-        posts
+        post: posts[id]
     }
 }
 export default connect(mapStateToProps)(PostDetailsView);
