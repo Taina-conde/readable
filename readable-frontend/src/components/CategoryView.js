bcc0a0daa6e490f02b0a9f2516eaccae133ff709
Taina Conde <tainabatistaconde@gmail.com>
2021-03-29 11:42:44 -0400

    feat: CategoryView - connect to redux store

--- readable-frontend/src/components/CategoryView.js
+++ readable-frontend/src/components/CategoryView.js
@@ -1,5 +1,12 @@
 import React from 'react'; 
+import { connect } from 'react-redux'
+
+import {Link} from 'react-router-dom'
+import Post from './Post'
 class CategoryView extends React.Component {
+    componentDidMount() {
+        
+    }
     render(){
         const {postsIds, posts} = this.props;
         console.log('homeview posts', posts)
@@ -8,17 +15,22 @@ class CategoryView extends React.Component {
 
             
             <ul className = "posts-list">
-                {postsIds.map((id) => (
+                {/*postsIds.map((id) => (
                     <li key = {id} className = 'post-list-item'>
                         <Link to = {`/${posts[id].category}/${id}`} className = 'post-list-link' >
                             <Post id = {id}/>
                         </Link>
                     </li>
                     
-                ))}
+                ))*/}
             </ul>
             </div>
         )
     }
 }
-export default CategoryView;
+function mapStateToProps({posts, categories}) {
+    return {
+
+    }
+}
+export default connect(mapStateToProps)(CategoryView);
