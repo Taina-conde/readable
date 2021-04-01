02fe659482174ae507774e1d8a6a38e14e54b5f7
Taina Conde <tainabatistaconde@gmail.com>
2021-04-01 16:02:09 -0400

    feat: PostButton - change comments button to Link
    Make that button a link to the PostDetailsView, but keep the styles of the
    other buttons.

--- readable-frontend/src/components/PostButtons.js
+++ readable-frontend/src/components/PostButtons.js
@@ -3,16 +3,19 @@ import {Row, Col} from 'react-bootstrap'
 import { IoChatbubblesOutline } from "react-icons/io5"
 import { BiLike, BiDislike, BiEdit, BiTrash } from 'react-icons/bi'
 import {connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 class PostButtons extends React.Component {
     render() {
+      
         const {post}= this.props;
+       
         return (
             <Row className = 'mt-2'>
                 <Col className = 'd-flex align-items-center'>
-                    <button type = 'button' className = 'post-btn col-2 d-flex'>
+                    <Link to = {`/${post.category}/${post.id}`} className = 'post-btn col-2 d-flex'>
                         <IoChatbubblesOutline className = 'text-success col-12 p-0' size = {20} />
-                    </button>
+                    </Link>
                     <span className = 'post-details col-9 p-0 ml-1'>{`${post.commentCount} ${post.commentCount === 1 ? 'comment': 'comments'}`}</span>
                 </Col>
                 <Col className = 'd-flex align-items-center'>
