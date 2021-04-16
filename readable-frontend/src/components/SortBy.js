881ed991bc940a635a7203c6d6954c929b3d70be
Taina Conde <tainabatistaconde@gmail.com>
2021-04-16 17:53:49 -0400

    fix: SortBy
    Remove selected from the first option and use
    defaultValue to avoid warnings.

--- readable-frontend/src/components/SortBy.js
+++ readable-frontend/src/components/SortBy.js
@@ -17,7 +17,7 @@ class SortBy extends React.Component {
                 
                 <span className = ''>Sort by: </span>
                 <select className = 'form-select' value = {this.state.value} onChange = {this.handleChange}>
-                    <option value = 'featured' selected>Featured</option>
+                    <option defaultValue = 'featured'>Featured</option>
                     <option value = 'most-recent' >Date (most recent first)</option>
                     <option value = 'less-recent'>Date (less recent first)</option>
                     <option value = 'highest-votes'>Votes (highest first)</option>
