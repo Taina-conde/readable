b70d4c21229de2025ba17c72f26399e8054b1aac
Taina Conde <tainabatistaconde@gmail.com>
2021-04-23 16:17:37 -0400

    style: Post - center text in each post detail

--- readable-frontend/src/components/Post.js
+++ readable-frontend/src/components/Post.js
@@ -15,19 +15,15 @@ class Post extends React.Component {
                         
                     </div>
                     <div className = 'col-12 col-md-6 post-details d-flex flex-wrap flex-sm-nowrap'>
-                        <span className = 'col-6 order-1 order-sm-0 col-sm-3 p-0'>
+                        <span className = 'col-6 order-1 order-sm-0 col-sm-4 p-0 text-md-center'>
                             {`${post.author}`}
                         </span> 
-                        <span className = 'col-sm-1 order-sm-1  d-none d-sm-inline'>
-                            •
-                        </span>
-                        <span className = 'col-6 order-3 order-sm-2 col-sm-4 p-0'>
+                        
+                        <span className = 'col-6 order-3 order-sm-2 col-sm-4 p-0 text-md-center'>
                             {formatDate(post.timestamp)}
                         </span>
-                        <span className = 'col-sm-1 order-sm-3 d-none d-sm-inline'>
-                            •
-                        </span>
-                        <span className = 'col-6 order-2 order-sm-4 col-sm-3 p-0 post-category'>
+                        
+                        <span className = 'col-6 order-2 order-sm-4 col-sm-4 p-0 post-category text-center'>
                             <span >{capitalize(post.category)}</span>
                         </span>
                         
