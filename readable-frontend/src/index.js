ffaea56a996e5493859887ea79fe16f8ccab2bb1
Taina Conde <tainabatistaconde@gmail.com>
2021-04-26 16:08:05 -0400

    refactor: Remove unused files
    Transfer content from App.css to styles.scss and delete App.css.
    Delete CategoryView component file, because the Categories are
    beeing rendered in HomeView.

--- readable-frontend/src/index.js
+++ readable-frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/css/styles.css';
-import './App.css';
 import App from './components/App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
