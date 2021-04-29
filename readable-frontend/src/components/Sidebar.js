9bf439f6d6913a7b2bc6d1730be5b108b9d52ef0
Taina Conde <tainabatistaconde@gmail.com>
2021-04-29 13:28:50 -0400

    refactor: Adjust formatting of Sidebar, SortBy

--- readable-frontend/src/components/Sidebar.js
+++ readable-frontend/src/components/Sidebar.js
@@ -1,74 +1,103 @@
-import React from 'react'
+import React from "react";
 import { HiAtSymbol } from "react-icons/hi";
-import {connect} from 'react-redux'
-import {capitalize} from '../utils/helpers'
-import { NavLink} from 'react-router-dom'
-import { handleCategoryPosts} from  '../actions/shared'
+import { connect } from "react-redux";
+import { capitalize } from "../utils/helpers";
+import { NavLink } from "react-router-dom";
+import { handleCategoryPosts } from "../actions/shared";
 import { IoClose } from "react-icons/io5";
 
 class Sidebar extends React.Component {
-    state = {
-        active: ""
+  state = {
+    active: "",
+  };
+  componentDidMount() {
+    const { categoriesNames, handleCategoryPosts } = this.props;
+    categoriesNames.forEach((category) => {
+      handleCategoryPosts(category);
+    });
+  }
+  handleCategoryClick = () => {
+    const vw = Math.max(
+      document.documentElement.clientWidth || 0,
+      window.innerWidth || 0
+    );
+    const { onHandleToggleSidebar } = this.props;
+    if (vw < 768) {
+      onHandleToggleSidebar();
     }
-    componentDidMount() {
-        const {categoriesNames, handleCategoryPosts} = this.props;
-        categoriesNames.forEach(category => {
-            handleCategoryPosts(category)
-        });
-        
-    }
-    handleCategoryClick = () =>{
-        const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-        const {onHandleToggleSidebar} = this.props;
-        if (vw < 768) {
-            onHandleToggleSidebar()
+  };
+  render() {
+    const {
+      categories,
+      categoriesNames,
+      onHandleToggleSidebar,
+      toggleSidebar,
+    } = this.props;
+
+    return (
+      <div
+        className={
+          toggleSidebar
+            ? "sidebar d-flex flex-column"
+            : "d-none d-md-flex flex-column sidebar"
         }
-    }
-    render() {
-        const{categories, categoriesNames, onHandleToggleSidebar, toggleSidebar} = this.props;
-        
-        return (
-            
-            <div className = {toggleSidebar ? "sidebar d-flex flex-column" : 'd-none d-md-flex flex-column sidebar'}>
-                <div className = 'col'>
-                    <a href = '/' className = "brand d-flex align-items-center text-success text-decoration-none">
-                        <HiAtSymbol size = {30}/> 
-                        <span className = "brand-text">PostIt</span>
-                        
-                    </a>
-                    <button  type= 'button' className = 'dismiss-btn d-md-none' onClick= {() => onHandleToggleSidebar()}>
-                            <IoClose size = {25}/>
-                    </button>
-                </div>
-                
-                <div className = 'col sidebar-hr'><hr/></div>
-                <ul className= "col nav nav-pills flex-column mb-auto">
-                    <li className = 'nav-item' onClick= {this.handleCategoryClick}>
-                        <NavLink exact className = 'nav-link' to = '/' activeClassName = "category-active">
-                                <span>All posts</span>
-                        </NavLink> 
-                    </li>
-                    {categoriesNames.map( category => (
-                        <li className = 'nav-item' key = {category} onClick= {this.handleCategoryClick}>
-                            <NavLink exact to = {`/${categories[category].path}/posts`} className = 'nav-link' activeClassName = "category-active">
-                                <span>{capitalize(category)}</span>
-                            </NavLink>   
-                        </li>
-                    ))}
+      >
+        <div className="col">
+          <a
+            href="/"
+            className="brand d-flex align-items-center text-success text-decoration-none"
+          >
+            <HiAtSymbol size={30} />
+            <span className="brand-text">PostIt</span>
+          </a>
+          <button
+            type="button"
+            className="dismiss-btn d-md-none"
+            onClick={() => onHandleToggleSidebar()}
+          >
+            <IoClose size={25} />
+          </button>
+        </div>
 
-                </ul>
-                
-                
-
-            </div>
-            
-        )
-    }
+        <div className="col sidebar-hr">
+          <hr />
+        </div>
+        <ul className="col nav nav-pills flex-column mb-auto">
+          <li className="nav-item" onClick={this.handleCategoryClick}>
+            <NavLink
+              exact
+              className="nav-link"
+              to="/"
+              activeClassName="category-active"
+            >
+              <span>All posts</span>
+            </NavLink>
+          </li>
+          {categoriesNames.map((category) => (
+            <li
+              className="nav-item"
+              key={category}
+              onClick={this.handleCategoryClick}
+            >
+              <NavLink
+                exact
+                to={`/${categories[category].path}/posts`}
+                className="nav-link"
+                activeClassName="category-active"
+              >
+                <span>{capitalize(category)}</span>
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
-function mapStateToProps({categories}) {
-    return {
-        categories,
-        categoriesNames : Object.keys(categories)
-    }
+function mapStateToProps({ categories }) {
+  return {
+    categories,
+    categoriesNames: Object.keys(categories),
+  };
 }
-export default connect(mapStateToProps, {handleCategoryPosts})(Sidebar);
+export default connect(mapStateToProps, { handleCategoryPosts })(Sidebar);
--- readable-frontend/src/components/SortBy.js
+++ readable-frontend/src/components/SortBy.js
@@ -1,32 +1,36 @@
-import React from 'react'
+import React from "react";
 
 class SortBy extends React.Component {
-    state = {
-        value: this.props.value,
-    }
-    handleChange = (event) => {
-        
-        this.setState(() => ({
-            value: event.target.value
-        }), ()=> { console.log(this.state.value) 
-            this.props.onHandleSort(this.state.value)})
-    }
-    render() {
-        return (
-            <div className = 'sort-box'>
-                
-                <span className = ''>Sort by: </span>
-                <select className = 'form-select' value = {this.state.value} onChange = {this.handleChange}>
-                    <option defaultValue = 'featured'>Featured</option>
-                    <option value = 'most-recent' >Date (most recent first)</option>
-                    <option value = 'less-recent'>Date (less recent first)</option>
-                    <option value = 'highest-votes'>Votes (highest first)</option>
-                    <option value = 'lowest-votes'>Votes (lowest first)</option>
-
-                </select>
-                
-            </div>
-        )
-    }
+  state = {
+    value: this.props.value,
+  };
+  handleChange = (event) => {
+    this.setState(
+      () => ({
+        value: event.target.value,
+      }),
+      () => {
+        this.props.onHandleSort(this.state.value);
+      }
+    );
+  };
+  render() {
+    return (
+      <div className="sort-box">
+        <span className="">Sort by: </span>
+        <select
+          className="form-select"
+          value={this.state.value}
+          onChange={this.handleChange}
+        >
+          <option defaultValue="featured">Featured</option>
+          <option value="most-recent">Date (most recent first)</option>
+          <option value="less-recent">Date (less recent first)</option>
+          <option value="highest-votes">Votes (highest first)</option>
+          <option value="lowest-votes">Votes (lowest first)</option>
+        </select>
+      </div>
+    );
+  }
 }
 export default SortBy;
